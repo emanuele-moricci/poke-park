1fd60ff1d31e6193e4069756606c679fd2313271
emanuele-moricci <emanuele.moricci@gmail.com>
2022-01-06 13:17:24 +0100

    Added the flip animations to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       },
       animation: {
         chill: 'chill 1s infinite;',
+        right: 'right 0.5s 1 cubic-bezier(0.8, 0, 1, 1) forwards;',
+        left: 'left 0.5s 1 cubic-bezier(0.8, 0, 1, 1) forwards;',
       },
       keyframes: {
         chill: {
@@ -20,6 +22,22 @@ module.exports = {
           '50%': {
             transform: 'translateY(0)',
             'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+        right: {
+          '0%': {
+            transform: 'rotateY(0deg)',
+          },
+          '100%': {
+            transform: 'rotateY(180deg)',
+          },
+        },
+        left: {
+          '0%': {
+            transform: 'rotateY(180deg)',
+          },
+          '100%': {
+            transform: 'rotateY(0deg)',
           },
         },
       },
