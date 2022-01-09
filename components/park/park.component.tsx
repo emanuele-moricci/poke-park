0a76669d637ca846c1a445939b1f7d3d1e2d0aba
emanuele-moricci <emanuele.moricci@gmail.com>
2022-01-09 14:54:28 +0100

    First tests for the Light/Dark mode switch

--- components/park/park.component.tsx
+++ components/park/park.component.tsx
@@ -8,24 +8,35 @@
 
  * This component is responsible for rendering the background image of the park.
  *
- * TODO: Add a random background image with a choice to change it.
+ * The current background can switch between `light` and `dark` mode
  *
  */
+import { useState } from 'react';
 import Image from 'next/image';
 
-import bgTest from 'assets/images/bg/pkmn-bg-1.jpg';
+import bgLight from 'assets/images/bg/pkmn-bg-1-light.jpg';
+import bgDark from 'assets/images/bg/pkmn-bg-1-dark.jpg';
 
 const Park = (): JSX.Element => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
   return (
-    <div className="absolute min-h-screen top-0 bottom-0 left-0 right-0 -z-10">
-      <Image
-        src={bgTest}
-        alt="BG Test"
-        layout="fill"
-        objectFit="cover"
-        priority
-      />
-    </div>
+    <>
+      <button
+        className="absolute top-[50%] left-[50%] z-50"
+        onClick={() => setDarkMode(!darkMode)}
+      >
+        CLICK MEH
+      </button>
+      <div className="fixed min-h-screen top-0 bottom-0 left-0 right-0 -z-10">
+        <Image
+          src={!darkMode ? bgLight : bgDark}
+          alt="BG Test"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+    </>
   );
 };
 
