d5fddab5d622ead26aafb0fffade8b17ba79e9c9
emanuele-moricci <emanuele.moricci@gmail.com>
2022-01-16 21:48:03 +0100

    Capitalized the Name in the 'Pokemon Info' Drawer

--- components/info/info.component.tsx
+++ components/info/info.component.tsx
@@ -47,7 +47,9 @@ const Drawer = () => {
         )}
       >
         <article className="relative w-screen max-w-lg pb-10 flex flex-col overflow-y-scroll h-full">
-          <header className="p-4 font-bold text-lg">{pkmn?.name}</header>
+          <header className="p-4 font-bold text-lg capitalize">
+            {pkmn?.name}
+          </header>
           {pkmn && <Content pkmn={pkmn} />}
         </article>
       </section>
