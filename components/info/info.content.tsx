3b0c9ea9794b9fe72dbbdeffb8fa58842643e250
emanuele-moricci <emanuele.moricci@gmail.com>
2022-01-16 17:18:53 +0100

    Added the first information for the interacted Pokemon in the 'Info Drawer'

--- components/info/info.content.tsx
+++ components/info/info.content.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import type { Pokemon } from 'pokenode-ts';
+import Image from 'next/image';
 
 interface IContentProps {
   pkmn: Pokemon;
 }
 
 const Content = ({ pkmn }: IContentProps) => {
-  console.log(pkmn);
-  return <></>;
+  const {
+    id,
+    name,
+    height,
+    weight,
+    sprites: { front_default, front_shiny },
+  } = pkmn;
+
+  return (
+    <div className="pl-4 flex flex-col items-start">
+      <div className="mb-14 flex flex-row items-center justify-center w-full">
+        <Image
+          src={front_default ?? ''}
+          alt={`${name} - Default`}
+          height="150px"
+          width="180px"
+        />
+        <Image
+          src={front_shiny ?? ''}
+          alt={`${name} - Shiny`}
+          height="150px"
+          width="180px"
+        />
+      </div>
+      <span>ID: {id}</span>
+      <span>Height: {height}</span>
+      <span>Weight: {weight}</span>
+    </div>
+  );
 };
 
 export default Content;
